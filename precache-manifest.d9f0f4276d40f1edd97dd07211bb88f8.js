db666e7a8193075ea3e866b6008ebfd20a4a34a8
Alexander <alexxx547@gmail.com>
2019-05-16 07:31:15 +0300

    Updates

---
+++ precache-manifest.d9f0f4276d40f1edd97dd07211bb88f8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/star-wars-db/static/media/Roboto.58bf8e0b.woff"
+  },
+  {
+    "revision": "eb45cc43dc1e437794aa",
+    "url": "/star-wars-db/static/css/main.93ba998a.chunk.css"
+  },
+  {
+    "revision": "9e323f3ec3bdc08f7554",
+    "url": "/star-wars-db/static/js/runtime~main.8275ed80.js"
+  },
+  {
+    "revision": "e081c66fffbc304b3a4a",
+    "url": "/star-wars-db/static/js/2.842214fc.chunk.js"
+  },
+  {
+    "revision": "69eee0b43d46be3ad91ef75a76ea3959",
+    "url": "/star-wars-db/static/media/Roboto.69eee0b4.woff2"
+  },
+  {
+    "revision": "38e1ed9a9ee49d755d20226b0d66588c",
+    "url": "/star-wars-db/static/media/Robotolight.38e1ed9a.woff2"
+  },
+  {
+    "revision": "4a34361b26e9896ea1b5065708f8daea",
+    "url": "/star-wars-db/static/media/Robotomedium.4a34361b.woff2"
+  },
+  {
+    "revision": "15a913e1228cca9b1247b373bdf27e55",
+    "url": "/star-wars-db/static/media/Robotobold.15a913e1.woff2"
+  },
+  {
+    "revision": "eb45cc43dc1e437794aa",
+    "url": "/star-wars-db/static/js/main.dfd6e88a.chunk.js"
+  },
+  {
+    "revision": "611ac98371c2d77c377041e1a0fd87dc",
+    "url": "/star-wars-db/static/media/Robotolight.611ac983.woff"
+  },
+  {
+    "revision": "7bf74824d7a8b3c08fe5956f93e9ce2a",
+    "url": "/star-wars-db/static/media/Robotobold.7bf74824.woff"
+  },
+  {
+    "revision": "6422eac71f54ee5cd007dcf46fc0d22d",
+    "url": "/star-wars-db/static/media/Robotomedium.6422eac7.woff"
+  },
+  {
+    "revision": "63d6f579b0db1fc75cc977a0539ce45f",
+    "url": "/star-wars-db/static/media/Robotolight.63d6f579.ttf"
+  },
+  {
+    "revision": "c613ca81d36649b260700f3335d8f579",
+    "url": "/star-wars-db/static/media/Roboto.c613ca81.ttf"
+  },
+  {
+    "revision": "38aa81d461c299fa9f0cb27726401996",
+    "url": "/star-wars-db/static/media/Robotomedium.38aa81d4.ttf"
+  },
+  {
+    "revision": "1fc6a83d1e366dfe7babb690d33ba6fb",
+    "url": "/star-wars-db/static/media/Robotobold.1fc6a83d.ttf"
+  },
+  {
+    "revision": "e7f819a47a35d997fc86842e1077d5ba",
+    "url": "/star-wars-db/index.html"
+  }
+];
