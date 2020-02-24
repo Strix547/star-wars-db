0a05740814334aa254a560669645d05f2750777d
Alexander <alexxx547@gmail.com>
2020-02-24 18:51:53 +0300

    fix error indicator and load spinner

--- src/components/ErrorIndicator/ErrorIndicator.js
+++ src/components/ErrorIndicator/ErrorIndicator.js
@@ -5,9 +5,9 @@ import './ErrorIndicator.scss'
 const ErrorIndicator = () => {
   return (
     <div className="error-indicator">
-      <span className="boom">BOOM!</span>
-      <span>something has gone terribly wrong</span>
-      <span>(but we already sent droids to fix it)</span>
+      <p className="boom">BOOM!</p>
+      <p>something has gone terribly wrong</p>
+      <p>(but we already sent droids to fix it)</p>
     </div>
   )
 }
